cfa0488b524da4592e91f045909a0eeb1646279c
Skinkan3000 <skynk3000@gmail.com>
2024-08-07 18:40:02 +0200

    Update page.

--- assets/js/loadcontent.js
+++ assets/js/loadcontent.js
@@ -1,8 +1,25 @@
-function loadHTML(file, elementId) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        })
-        .catch(error => console.error('Error loading the HTML file:', error));
+function loadHTML(url, elementId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById(elementId).innerHTML = xhr.responseText;
+            executeScripts(document.getElementById(elementId));
+        }
+    };
+    xhr.send();
+}
+
+function executeScripts(element) {
+    var scripts = element.getElementsByTagName('script');
+    for (var i = 0; i < scripts.length; i++) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        if (scripts[i].src) {
+            script.src = scripts[i].src;
+        } else {
+            script.innerHTML = scripts[i].innerHTML;
+        }
+        document.head.appendChild(script);
+    }
 }
