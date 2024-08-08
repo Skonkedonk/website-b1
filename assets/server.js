f96519539b1a1aac3cbe007d7cebc828adb888f8
Skinkan3000 <skynk3000@gmail.com>
2024-08-08 22:37:13 +0200

    Update page.

--- assets/server.js
+++ assets/server.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/graphql', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
