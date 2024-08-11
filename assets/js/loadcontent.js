9179c56db57e3b8f3c28cb61ea853daf46f3d036
Skinkan3000 <skynk3000@gmail.com>
2024-08-11 12:44:53 +0200

    Update page.

--- assets/js/loadcontent.js
+++ assets/js/loadcontent.js
@@ -1,32 +1,47 @@
 function loadHTML(url, elementId) {
+    console.log(`Loading HTML from ${url} into element with ID ${elementId}`);
+    
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
+    
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var element = document.getElementById(elementId);
-            if (element) {
-                element.innerHTML = xhr.responseText;
-                executeScripts(element);
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log(`Successfully loaded HTML from ${url}`);
+                
+                var element = document.getElementById(elementId);
+                if (element) {
+                    element.innerHTML = xhr.responseText;
+                    console.log(`HTML content successfully injected into element with ID ${elementId}`);
+                    executeScripts(element);
+                } else {
+                    console.error(`Element with ID ${elementId} not found.`);
+                }
             } else {
-                console.error(`Element with ID ${elementId} not found.`);
+                console.error(`Failed to load ${url}. Status: ${xhr.status}`);
             }
-        } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            console.error(`Failed to load ${url}. Status: ${xhr.status}`);
         }
     };
+    
     xhr.send();
 }
 
 function executeScripts(element) {
     var scripts = element.getElementsByTagName('script');
+    console.log(`Executing ${scripts.length} scripts in the loaded content`);
+    
     for (var i = 0; i < scripts.length; i++) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
+        
         if (scripts[i].src) {
             script.src = scripts[i].src;
+            console.log(`Loading script from ${scripts[i].src}`);
         } else {
             script.innerHTML = scripts[i].innerHTML;
+            console.log('Executing inline script');
         }
+        
         document.head.appendChild(script);
     }
 }
